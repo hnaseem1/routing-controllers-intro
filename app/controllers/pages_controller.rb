cd1d387e4afcc5a8a564224b083a4823d73b903b
Hasan Naseem <hn92@live.com>
2018-04-12 14:46:30 -0400

    defined @header variables inside the controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,16 @@
 class PagesController < ApplicationController
   def welcome
+    @header = "This is the welcome page"
     render :welcome
   end
 
   def about
+    @header = "This is the about page"
     render :about
+  end
+
+  def contest
+    @header = "This is a contest page"
+    render :contest
   end
 end
